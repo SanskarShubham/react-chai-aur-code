44b7e245f0b6f271a9524b5657305a13a36aacb3
shubham ojha <shubhamkumarojha9852@gmail.com>
2024-05-01 12:52:47 +0530

    bgChanger project complete

--- bgChanger/src/App.jsx
+++ bgChanger/src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+    const [theme, setTheme] = useState('light');
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      {/* // i have to change the background theme by clicking multiple button design UI using tailwind */}
+
+      <div className={`w-full h-screen duration-200  p-4 bg-${theme}-500`}>
+      <div className="flex fex-wrap  justify-center items-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl " >
+        <button
+          className=" outline-none  py-2 px-4 rounded bg-blue-500 hover:bg-blue-700 text-white font-bold"
+          onClick={() => setTheme('blue')}
+          
+        >
+          Blue
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setTheme('red')}
+        >
+          Red
+        </button>
+        <button
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setTheme('green')}
+        >
+          Green
+        </button>
+        <button
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => setTheme('yellow')}
+        >
+          Yellow
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
     </>
   )
 }
